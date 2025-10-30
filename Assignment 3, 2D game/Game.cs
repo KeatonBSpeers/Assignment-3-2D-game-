c30dae9940c906c924b016cc1a107ad12fda1609
MightBeKaz-YouNeverKnow <000972147@mohawkcollege.ca>
2025-10-30 14:26:01 -0400

    Update Game.cs

--- Assignment 3, 2D game/Game.cs
+++ Assignment 3, 2D game/Game.cs
@@ -10,17 +10,28 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-        float playerX = 100;
+        float playerX = 600;
         float playerY = 100;
+        // Opponent Variables
+        float oppenentX = 200;
+        float oppenentY = 100;
         // movement speed in pixels per second
         float speed = 200f;
+        //Ball Variables
+        float ballX = 400;
+        float ballY = 300;
+
+        float velocityY = 0f;
+        float gravity = 1500f;     // pixels per second^2
+       
+
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-            Window.SetTitle("Assignment 3 - Keaton");
+            Window.SetTitle("Assignment 3 - Pong Keaton");
             Window.SetSize(800, 600);
             Color Red = new Color(255, 0, 0);
             Window.SetFpsToMonitorRefreshRate();
@@ -32,6 +43,7 @@ namespace MohawkGame2D
         public void Update()
         {
             Window.ClearBackground(Color.Black);
+            Draw.Rectangle(oppenentX, oppenentY, 25, 130);
 
             // delta time for frame-independent movement
             float dt = Raylib.GetFrameTime();
@@ -45,7 +57,7 @@ namespace MohawkGame2D
             {
                 playerY += speed * dt;
             }
-           
+
             //Self Destruct Buttons
             if (Raylib.IsKeyDown(KeyboardKey.PageUp))
             {
@@ -62,7 +74,11 @@ namespace MohawkGame2D
             playerY = Math.Clamp(playerY, radius, Raylib.GetScreenHeight() - radius);
 
             Draw.FillColor = Color.OffWhite;
-            Draw.Rectangle(playerX, playerY,25, 130);
+            Draw.Rectangle(playerX, playerY, 25, 130);
+            {
+                Draw.Circle(ballX, ballY, 20);
+
+            }
         }
     }
 }
